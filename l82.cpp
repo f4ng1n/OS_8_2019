38e632ae3b952ff8c0dd8e5300f6544884f147b8
Phan Thi Hai Ha <phantransmith@gmail.com>
2019-05-22 19:42:46 +0300

    Update l82.cpp

--- l82.cpp
+++ l82.cpp
@@ -13,33 +13,37 @@ using namespace std;
 int fd;   
 bool flag1 = true;
 
-
-void* func1(void *) {
-    
-cout << "Reading from named channel:"<<endl;
+void* func1(void *) 
+{    
+    cout << "Reading from named channel:"<<endl;
     int buf;
-while(flag1){
-
+    while(flag1)
+    {
        buf = 0;
-      int pr = read(fd,(void*)&buf,sizeof(buf));
-     if (pr > 0)
+       int pr = read(fd,(void*)&buf,sizeof(buf));
+       if (pr > 0)
        cout<<buf<<flush<<endl;    
-     else
+       else
         {
          flag1 = 0;
-        cout <<"Press ENTER"<<endl; 
-   }
- }                  
+         cout <<"Press ENTER"<<endl; 
+        }
+    }                  
 }
 
-int main() {
+int main() 
+{
     pthread_t id1;
     mkfifo("canal",0777);
+    
     fd=open("canal",O_RDONLY);
+    
     pthread_t args;
     flag1=true;
+    
     pthread_create(&id1, NULL, func1,(void*)&args);
     getchar();
+    
     flag1 = 0;
     pthread_join(id1, NULL);
     close(fd);

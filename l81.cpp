02842bfc836a5c8671408e5c3d36547c762704e0
Phan Thi Hai Ha <phantransmith@gmail.com>
2019-05-22 19:41:29 +0300

    Update l81.cpp

--- l81.cpp
+++ l81.cpp
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <string>
 #include <string.h>
 #include <stdlib.h>
 #include <csignal>
@@ -24,45 +23,40 @@ void sig_handle (int signo)
 }
 
 
-void* func1(void *) {
-    
-cout << "Writing to named channel:"<<endl;
-  
+void* func1(void *) 
+{    
+cout << "Writing to named channel:"<<endl;  
 int buf = 0;
-   while(flag1){
-
-      buf++;
-      
+   while(flag1)
+   {
+     buf++;      
      int pr = write(fd,(void*)&buf,sizeof(int));
-    if (pr > 0)
+     if (pr > 0)
        cout<<buf<<flush;
        cout<<endl;
-        sleep(1);
-  }
+       sleep(1);
+   }
 }
 
-
-
-
-int main() {
-	
-    pthread_t id1;
+int main() 
+{	
+   pthread_t id1;
    signal (SIGPIPE, sig_handle); 
     
-  mkfifo("canal",0777);
-
+   mkfifo("canal",0777);
   
-  fd=open("canal",O_WRONLY|O_NONBLOCK);
-  if (fd < 0)
+   fd=open("canal",O_WRONLY|O_NONBLOCK);
+   if (fd < 0)
        {
 	cout << "No process to read!" << endl;
 	return 0;
-		}
+	}
     
     pthread_t args;
     flag1=true;
     pthread_create(&id1, NULL, func1,(void*)&args);
     getchar();
+	
     flag1 = 0;
     pthread_join(id1, NULL);
     
